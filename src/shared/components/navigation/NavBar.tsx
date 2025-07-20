96e4c811d94c137923a6e5b97548310512091f07
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-07-20 14:56:57 +0200

    added navigation bar files

---
+++ src/shared/components/navigation/NavBar.tsx
@@ -0,0 +1,12 @@
+import styles from "./navbar.module.scss"
+
+export const NavBar = () => {
+    return(
+        <div>
+            <div></div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
