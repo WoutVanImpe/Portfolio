40f27e6492db3b6fea16e3b81d032d6e1e1a27ee
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-07-20 14:29:42 +0200

    added divider shape

--- src/home/Home.page.tsx
+++ src/home/Home.page.tsx
@@ -23,15 +23,13 @@ export const Home = () => {
 		mass: 1,
 	});
 
-
 	const gradColor1 = useTransform(smoothScrollY, [0.5, 0.8], ["#4c24e1", "#4c24e1"]);
 	const gradColor2 = useTransform(smoothScrollY, [0.5, 0.8], ["#0065f4", "#f200a0"]);
 	const gradColor3 = useTransform(smoothScrollY, [0.5, 0.8], ["#007cd3", "#ff495f"]);
 	const gradColor4 = useTransform(smoothScrollY, [0.5, 0.8], ["#0088a0", "#ffad3f"]);
 	const gradColor5 = useTransform(smoothScrollY, [0.5, 0.8], ["#008f7a", "#f9f871"]);
 
-	const gradient = useTransform(smoothScrollY, (value) => `linear-gradient(to right top, ${gradColor1.get()}, ${gradColor2.get()}, ${gradColor3.get()}, ${gradColor4.get()}, ${gradColor5.get()})`)
-
+	const gradient = useTransform(smoothScrollY, (value) => `linear-gradient(to right top, ${gradColor1.get()}, ${gradColor2.get()}, ${gradColor3.get()}, ${gradColor4.get()}, ${gradColor5.get()})`);
 
 	return (
 		<div className={styles["p-home"]}>
@@ -49,7 +47,16 @@ export const Home = () => {
 						<h2>Ik ben Wout Van Impe</h2>
 					</div>
 				</div>
+				<div className={styles["custom-shape-divider-bottom-1753013852"]}>
+					<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+						<path
+							d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+							className={styles["shape-fill"]}
+						></path>
+					</svg>
+				</div>
 			</motion.div>
+
 			<div style={{ height: "1000px" }}></div>
 		</div>
 	);
