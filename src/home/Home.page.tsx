764e195ab2613d0813518ffbaa588ac4f3664c38
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-07-20 13:55:11 +0200

    added parallax gradient to header

--- src/home/Home.page.tsx
+++ src/home/Home.page.tsx
@@ -3,6 +3,8 @@ import { useGetProjects } from "~shared/hooks/projects-data/useGetProjects.hooks
 import type ProjectType from "~shared/hooks/projects-data/project.types";
 import useWindowDimensions from "~shared/hooks/screen-size/useWindowDimensions";
 import { FloatingCardToLeft, FloatingCardToRight } from "./components/FloatingCard";
+import { useRef } from "react";
+import { motion, useScroll, useSpring, useTransform } from "motion/react";
 
 export const Home = () => {
 	const { data } = useGetProjects();
@@ -13,9 +15,27 @@ export const Home = () => {
 		while (projectList.length <= (dimensions.width / 253) * 2) data.forEach((project) => projectList.push(project));
 	}
 
+	const scrollRef = useRef<HTMLDivElement>(null);
+	const { scrollYProgress } = useScroll({ target: scrollRef, offset: ["start end", "end start"] });
+	const smoothScrollY = useSpring(scrollYProgress, {
+		stiffness: 50,
+		damping: 20,
+		mass: 1,
+	});
+
+
+	const gradColor1 = useTransform(smoothScrollY, [0.5, 0.8], ["#4c24e1", "#4c24e1"]);
+	const gradColor2 = useTransform(smoothScrollY, [0.5, 0.8], ["#0065f4", "#f200a0"]);
+	const gradColor3 = useTransform(smoothScrollY, [0.5, 0.8], ["#007cd3", "#ff495f"]);
+	const gradColor4 = useTransform(smoothScrollY, [0.5, 0.8], ["#0088a0", "#ffad3f"]);
+	const gradColor5 = useTransform(smoothScrollY, [0.5, 0.8], ["#008f7a", "#f9f871"]);
+
+	const gradient = useTransform(smoothScrollY, (value) => `linear-gradient(to right top, ${gradColor1.get()}, ${gradColor2.get()}, ${gradColor3.get()}, ${gradColor4.get()}, ${gradColor5.get()})`)
+
+
 	return (
 		<div className={styles["p-home"]}>
-			<div className={styles["p-home__header"]}>
+			<motion.div ref={scrollRef} className={styles["p-home__header"]} style={{ backgroundImage: gradient }}>
 				{projectList.map((project: ProjectType, index: number) => {
 					if (index % 2) {
 						return <FloatingCardToLeft key={project.title + index} project={project} index={index} containerWidth={dimensions.width} containerHeight={dimensions.height} />;
@@ -29,7 +49,8 @@ export const Home = () => {
 						<h2>Ik ben Wout Van Impe</h2>
 					</div>
 				</div>
-			</div>
+			</motion.div>
+			<div style={{ height: "1000px" }}></div>
 		</div>
 	);
 };
