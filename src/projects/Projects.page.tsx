2060b6aa3c9c7586c04b8a400e4aaafcfc5a6347
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-07-21 20:38:32 +0200

    project list forwarded to projects page

--- src/projects/Projects.page.tsx
+++ src/projects/Projects.page.tsx
@@ -1,7 +1,12 @@
 import { forwardRef } from "react";
 import styles from "./projects.module.scss";
+import type ProjectType from "~shared/hooks/projects-data/project.types";
 
-export const Projects = forwardRef<HTMLDivElement, {}>((props, ref) => {
+interface ProjectListProps {
+	projects: ProjectType[];
+}
+
+export const Projects = forwardRef<HTMLDivElement, ProjectListProps>(({ projects }, ref) => {
 	return (
 		<div ref={ref}>
 			<h1>Projects</h1>
