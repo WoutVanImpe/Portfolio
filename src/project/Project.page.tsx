77e9fcebd0318a153a9e2872047f4983e5a53223
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-07-27 11:36:33 +0200

    added base structure

--- src/project/Project.page.tsx
+++ src/project/Project.page.tsx
@@ -1,5 +1,24 @@
+import { useParams } from "react-router";
 import styles from "./project.module.scss";
+import { useProjects } from "~context/ProjectContext";
+import { PageTitle } from "~shared/hooks/page-title/PageTitle";
+import { ProjectContent } from "./components/project-content/ProjectContent";
 
 export const Project = () => {
+	const { id } = useParams<{ id: string }>();
+	const { projects } = useProjects();
+
+	if (projects) {
+		if (id) {
+			const project = projects.find((p) => p.id === parseInt(id));
+			return (
+				<>
+					<PageTitle title={`Wout Van Impe | ${project?.title}`} />
+					<ProjectContent {...project} />
+				</>
+			);
+		}
+	}
+
 	return <div></div>;
 };
