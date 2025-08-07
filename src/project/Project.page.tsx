591be12d45a0d00070c17bc9fd8dc43ff0e21c50
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-08-07 14:52:16 +0200

    fixed small preload bug

--- src/project/Project.page.tsx
+++ src/project/Project.page.tsx
@@ -1,20 +1,16 @@
 import { useParams } from "react-router";
 import styles from "./project.module.scss";
-import { useProjects } from "~context/ProjectContext";
+import { useData } from "~context/DataContext";
 import { PageTitle } from "~shared/hooks/page-title/PageTitle";
 
 export const ProjectPage = () => {
 	const { id } = useParams<{ id: string }>();
-	const { projects } = useProjects();
+	const { projects } = useData();
 
 	if (projects) {
 		if (id) {
 			const project = projects.find((p) => p.id === parseInt(id));
-			return (
-				
-					<PageTitle title={`Wout Van Impe | ${project?.title}`} />
-				
-			);
+			return <PageTitle title={`Wout Van Impe | ${project?.title}`} />;
 		}
 	}
 
