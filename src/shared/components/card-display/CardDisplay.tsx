027c347c929ea0d6bc43e3faed2fb0edf87a75e8
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-08-09 21:45:14 +0200

    added hover effect to postcard

--- src/shared/components/card-display/CardDisplay.tsx
+++ src/shared/components/card-display/CardDisplay.tsx
@@ -2,15 +2,23 @@ import styles from "./carddisplay.module.scss";
 import frontFrameImg from "../assets/cardholder-front.svg";
 import backFrameImg from "../assets/cardholder-back.svg";
 import { Postcard } from "../postcard/Postcard";
+import { motion } from "motion/react";
 
 export const CardDisplay = () => {
 	return (
 		<div className={styles["postcard-display-container"]}>
 			<img className={styles["postcard-display-container__back"]} src={backFrameImg} alt="back of frame" />
-            <div className={styles["postcard-display-container__card"]}>
-                <Postcard/>
-            </div>
-            <img className={styles["postcard-display-container__front"]} src={frontFrameImg} alt="front of frame" />
+			<motion.div
+				className={styles["postcard-display-container__card"]}
+				style={{ rotateX: "5deg" }}
+				whileHover={{
+					rotateX: "0deg",
+					y: -20,
+				}}
+			>
+				<Postcard />
+			</motion.div>
+			<img className={styles["postcard-display-container__front"]} src={frontFrameImg} alt="front of frame" />
 		</div>
 	);
 };
