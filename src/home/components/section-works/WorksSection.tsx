e2e4fba3aa75a174b7e011f294a38e96a4f7a6f1
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-08-13 08:05:32 +0200

    added postcards to the works section

--- src/home/components/section-works/WorksSection.tsx
+++ src/home/components/section-works/WorksSection.tsx
@@ -1,9 +1,31 @@
+import { CardDisplay } from "~shared/components/card-display/CardDisplay";
 import styles from "./worksSection.module.scss";
+import useWindowDimensions from "~shared/hooks/screen-size/useWindowDimensions";
+import { motion, useMotionValue } from "motion/react";
+import { useEffect } from "react";
 
 export const WorksSection = () => {
+	const { width } = useWindowDimensions();
+	const cardScale = useMotionValue<number>(width > 1200 ? (1200 * 0.48) / 650 : (width * 0.48) / 650);
+
+	useEffect(() => {
+		cardScale.set(width > 1200 ? (1200 * 0.48) / 650 : (width * 0.48) / 650);
+	}, [width]);
+
 	return (
 		<div className={styles["s-works"]}>
-			<div></div>
+			<h1>Works</h1>
+			<motion.div className={styles["s-works__works-container"]}>
+				<motion.div className={styles["s-works__works-container__card-container"]} style={{ scale: cardScale }}>
+					<CardDisplay />
+				</motion.div>
+				<motion.div className={styles["s-works__works-container__card-container"]} style={{ scale: cardScale }}>
+					<CardDisplay />
+				</motion.div>
+				<motion.div className={styles["s-works__works-container__card-container"]} style={{ scale: cardScale }}>
+					<CardDisplay />
+				</motion.div>
+			</motion.div>
 		</div>
 	);
 };
