d5e609c38362562325d52b8d344bcfb4df6ef375
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-08-15 20:13:53 +0200

    changed the background color

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -3,23 +3,23 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 type ThemeContextType = {
 	darkmode: boolean;
 	setDarkmode: (mode: boolean) => void;
-	backgroundColor: "#aa88e7ff" | "#342353ff";
-	setBackgroundColor: (mode: "#aa88e7ff" | "#342353ff") => void;
+	backgroundColor: "#b89fe7ff" | "#342353ff";
+	setBackgroundColor: (mode: "#b89fe7ff" | "#342353ff") => void;
 };
 
 const ThemeContext = createContext<ThemeContextType>({
 	darkmode: false,
 	setDarkmode: () => {},
-	backgroundColor: "#aa88e7ff",
+	backgroundColor: "#b89fe7ff",
 	setBackgroundColor: () => {},
 });
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
 	const [darkmode, setDarkmode] = useState<boolean>(false);
-	const [backgroundColor, setBackgroundColor] = useState<"#aa88e7ff" | "#342353ff">("#aa88e7ff");
+	const [backgroundColor, setBackgroundColor] = useState<"#b89fe7ff" | "#342353ff">("#b89fe7ff");
 
 	useEffect(() => {
-		setBackgroundColor(darkmode ? "#342353ff" : "#aa88e7ff");
+		setBackgroundColor(darkmode ? "#342353ff" : "#b89fe7ff");
 	}, [darkmode]);
 
 	const value = useMemo(
