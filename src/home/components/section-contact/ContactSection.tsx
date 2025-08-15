7dc938cca0e7ae9e1c7c758c3946cf64e22ad729
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-08-16 00:38:49 +0200

    added contact section title

--- src/home/components/section-contact/ContactSection.tsx
+++ src/home/components/section-contact/ContactSection.tsx
@@ -1,9 +1,15 @@
+import { motion } from "motion/react";
 import styles from "./contactSection.module.scss";
+import { useTheme } from "~context/ThemeContext";
 
 export const ContactSection = () => {
+	const { textColor, textBgColor, textBorderColor } = useTheme();
+
 	return (
 		<div className={styles["s-contact"]}>
-			<div></div>
+			<motion.h1 animate={{ color: textColor, backgroundColor: textBgColor, borderColor: textBorderColor }} transition={{ duration: 1, ease: "easeIn" }}>
+				Contact
+			</motion.h1>
 		</div>
 	);
 };
