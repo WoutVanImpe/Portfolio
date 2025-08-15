0c233a54492330560009eb9cb840b426eebcbb27
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-08-15 20:13:44 +0200

    removed the second shelf and moved the lamp to the window

--- src/home/components/section-header/HeaderSection.tsx
+++ src/home/components/section-header/HeaderSection.tsx
@@ -1,20 +1,15 @@
-import { Window } from "~shared/components/window/Window";
 import styles from "./headerSection.module.scss";
 import useWindowDimensions from "~shared/hooks/screen-size/useWindowDimensions";
 import { motion, useMotionValue } from "motion/react";
 import { useEffect } from "react";
-import { Lamp } from "~shared/components/lamp/Lamp";
-import { Globe } from "~shared/components/globe/Glode";
+import { WindowLamp } from "~shared/components/window-lamp/WindowLamp";
 
 export const HeaderSection = () => {
 	const { width } = useWindowDimensions();
 	const windowScale = useMotionValue<number>(width > 1200 ? (1200 * 0.3) / 500 : (width * 0.3) / 500);
-	const shelfScale = useMotionValue<number>(width > 1200 ? (1200 * 0.4) / 500 : (width * 0.4) / 500);
-	const objectScale = useMotionValue<number>(width > 1200 ? (1200 * 0.15) / 200 : (width * 0.15) / 200);
 
 	useEffect(() => {
 		windowScale.set(width > 1200 ? (1200 * 0.3) / 400 : (width * 0.3) / 400);
-		shelfScale.set(width > 1200 ? (1200 * 0.4) / 500 : (width * 0.4) / 500);
 	}, [width]);
 
 	return (
@@ -24,21 +19,10 @@ export const HeaderSection = () => {
 					<h1>Hallo, ik ben Wout!</h1>
 					<h3>Student Multimedia en Creatieve Technologie</h3>
 				</div>
-				<div className={styles["s-header__greet-container__shelf-container"]}>
-					<div className={styles["s-header__greet-container__shelf-container__objects"]}>
-						<motion.div className={styles["s-header__greet-container__shelf-container__objects__lamp"]} style={{ scale: objectScale }}>
-							<Lamp />
-						</motion.div>
-						<motion.div className={styles["s-header__greet-container__shelf-container__objects__globe"]} style={{ scale: objectScale }}>
-							<Globe />
-						</motion.div>
-					</div>
-					<motion.div className={styles["s-header__greet-container__shelf-container__shelf"]} style={{ scale: shelfScale }}></motion.div>
-				</div>
 			</div>
 			<div className={styles["s-header__window-container"]}>
 				<motion.div className={styles["s-header__window-container__window"]} style={{ scale: windowScale }}>
-					<Window />
+					<WindowLamp />
 				</motion.div>
 			</div>
 		</div>
---
+++ src/shared/components/window-lamp/WindowLamp.tsx
@@ -0,0 +1,14 @@
+import { Lamp } from "../lamp/Lamp";
+import { Window } from "../window/Window";
+import styles from "./windowLamp.module.scss";
+
+export const WindowLamp = () => {
+	return (
+		<div className={styles["window-lamp-container"]}>
+			<Window />
+			<div className={styles["window-lamp-container__lamp"]}>
+				<Lamp />
+			</div>
+		</div>
+	);
+};
