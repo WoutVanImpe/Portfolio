1f3b1616ea2327fb6d46e524bd701bb48029cdab
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-08-15 23:30:52 +0200

    added title for the about section

--- src/home/components/section-about/AboutSection.tsx
+++ src/home/components/section-about/AboutSection.tsx
@@ -1,9 +1,15 @@
+import { useTheme } from "~context/ThemeContext";
 import styles from "./aboutSection.module.scss";
+import { motion } from "motion/react";
 
 export const AboutSection = () => {
+	const { textColor, textBgColor, textBorderColor } = useTheme();
+
 	return (
 		<div className={styles["s-about"]}>
-			<div></div>
+			<motion.h1 animate={{ color: textColor, backgroundColor: textBgColor, borderColor: textBorderColor }} transition={{ duration: 1, ease: "easeIn" }}>
+				About
+			</motion.h1>
 		</div>
 	);
 };
