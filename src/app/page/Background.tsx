2fdd365c2df52eb2e665e1a9b6bc23eae212c578
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-08-23 23:59:10 +0200

    added pattern back to background

--- src/app/page/Background.tsx
+++ src/app/page/Background.tsx
@@ -1,18 +1,34 @@
 import { motion } from "motion/react";
 import styles from "./headpage.module.scss";
 import { useTheme } from "~context/ThemeContext";
+import classNames from "classnames";
 
 export const Background = ({ children }: { children: React.ReactNode }) => {
-	const { backgroundColor } = useTheme();
+	const { backgroundColor, darkmode } = useTheme();
 
 	return (
-		<motion.div
-			className={styles["background"]}
-			animate={{
-				backgroundColor: backgroundColor,
-			}}
-			transition={{ duration: 1, ease: "easeIn" }}
-		>
+		<motion.div className={styles["background-container"]}>
+			<motion.div
+				className={styles["background"]}
+				animate={{
+					backgroundColor: backgroundColor,
+				}}
+				transition={{ duration: 1, ease: "easeIn" }}
+			></motion.div>
+			<motion.div
+				className={classNames(styles["background__pattern"], styles["background__pattern--light"])}
+				animate={{
+					opacity: darkmode ? 0 : 1,
+				}}
+				transition={{ duration: 1, ease: "easeIn" }}
+			></motion.div>
+			<motion.div
+				className={classNames(styles["background__pattern"], styles["background__pattern--dark"])}
+				animate={{
+					opacity: darkmode ? 1 : 0,
+				}}
+				transition={{ duration: 1, ease: "easeIn" }}
+			></motion.div>
 			{children}
 		</motion.div>
 	);
