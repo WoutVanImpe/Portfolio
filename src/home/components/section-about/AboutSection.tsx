b25718ef165c5918d6dba70d0137b787bf532ff2
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-09-03 23:34:04 +0200

    added about section

--- src/home/components/section-about/AboutSection.tsx
+++ src/home/components/section-about/AboutSection.tsx
@@ -4,6 +4,7 @@ import { motion } from "motion/react";
 import { forwardRef } from "react";
 import { Trans } from "react-i18next";
 import { Phone } from "~shared/components/phone/Phone";
+import { Clock } from "~shared/components/clock/Clock";
 
 export const AboutSection = forwardRef<HTMLDivElement, {}>((props, ref) => {
 	const { textColor, textBgColor, textBorderColor } = useTheme();
@@ -13,6 +14,16 @@ export const AboutSection = forwardRef<HTMLDivElement, {}>((props, ref) => {
 			<motion.h1 animate={{ color: textColor, backgroundColor: textBgColor, borderColor: textBorderColor }} transition={{ duration: 1, ease: "easeIn" }}>
 				<Trans>about.title</Trans>
 			</motion.h1>
+			<div className={styles["s-about__long-text"]}>
+				<motion.div className={styles["s-about__long-text__clock-container"]} style={{scale:1.7}}>
+					<Clock/>
+				</motion.div>
+				<motion.p animate={{ color: textColor }} transition={{ duration: 1, ease: "easeIn" }}>
+					Hi! Ik ben Wout, een derdejaarsstudent Multimedia en Creatieve Technologie aan de Erasmushogeschool Brussel, met een sterke passie voor front-end development en motion design. Ik experimenteer graag met nieuwe technologieën en daag mezelf
+					uit om creatief en oplossingsgericht te werken. Naast mijn studie ben ik actief als socialmediamanager bij JNM (Jeugdbond voor Natuur en Milieu), waar ik onder andere meewerk aan promomateriaal zoals Instagramcontent, flyers en magazines.
+					Verder besteed ik in mijn vrije tijd graag aandacht aan het aanscherpen van mijn technische en visuele skills.
+				</motion.p>
+			</div>
 		</div>
 	);
 });
