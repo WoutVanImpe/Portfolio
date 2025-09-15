93c73cd681ac683dd298338380c9a69ae677ae1e
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-09-15 22:56:52 +0200

    added closing animation to the envelope

--- src/shared/components/letter/Letter.tsx
+++ src/shared/components/letter/Letter.tsx
@@ -3,21 +3,25 @@ import frontImg from "../assets/letter-front.svg";
 import backImg from "../assets/letter-back.svg";
 import flapImg from "../assets/letter-flap.svg";
 import { useRef } from "react";
-import { motion } from "motion/react";
+import { AnimatePresence, motion, useAnimation } from "motion/react";
 import { Trans } from "react-i18next";
 
 export const Letter = () => {
-
 	const nameInput = useRef<HTMLInputElement | null>(null);
 	const emailInput = useRef<HTMLInputElement | null>(null);
 	const messageInput = useRef<HTMLTextAreaElement | null>(null);
 
-	const handleSend = () => {
+	const letterControls = useAnimation();
+	const envelopeControls = useAnimation();
+	const messageControls = useAnimation();
+
+	const handleSend = async () => {
 		const letter = {
 			name: nameInput.current?.value,
 			email: emailInput.current?.value,
 			message: messageInput.current?.value,
 		};
+		await closeEnvelope();
 	};
 
 	const handleMsgTyping = () => {
@@ -27,32 +31,52 @@ export const Letter = () => {
 		}
 	};
 
+	const closeEnvelope = async () => {
+		await Promise.all([
+			messageControls.start({
+				opacity: 0,
+				height: 0,
+				transition: { duration: 0.5, ease: "easeInOut" },
+			}),
+			letterControls.start({
+				y: 150,
+				transition: { duration: 0.8, ease: "easeInOut" },
+			}),
+		]);
+
+		await envelopeControls.start({
+			rotateX: 180,
+			zIndex: 5,
+			transition: { duration: 0.8, ease: "easeInOut" },
+		});
+	};
+
 	return (
 		<div className={styles["letter"]}>
-			<div className={styles["letter__paper"]}>
-				<motion.p >
-					<Trans>contact.nameInput</Trans> <motion.input name="name"  ref={nameInput} type="text" />
+			<motion.div className={styles["letter__paper"]} animate={letterControls}>
+				<motion.p>
+					<Trans>contact.nameInput</Trans> <motion.input name="name" ref={nameInput} type="text" />
 				</motion.p>
 
-				<motion.p >
-					<Trans>contact.emailInput</Trans> <motion.input name="email"  ref={emailInput} type="text" />
+				<motion.p>
+					<Trans>contact.emailInput</Trans> <motion.input name="email" ref={emailInput} type="text" />
 				</motion.p>
 
-				<motion.p >
+				<motion.p>
 					<Trans>contact.messageInput</Trans>
 				</motion.p>
-				<motion.div className={styles["letter__paper__message-wrapper"]}>
-					<motion.textarea name="message" className={styles["letter__paper__message-wrapper__message"]} rows={1} onInput={handleMsgTyping}  ref={messageInput} />
+				<motion.div className={styles["letter__paper__message-wrapper"]} animate={messageControls}>
+					<motion.textarea name="message" className={styles["letter__paper__message-wrapper__message"]} rows={1} onInput={handleMsgTyping} ref={messageInput} />
 				</motion.div>
-				<motion.button  onClick={handleSend}>
+				<motion.button onClick={handleSend}>
 					<Trans>contact.sendButton</Trans>
 				</motion.button>
-			</div>
+			</motion.div>
 			<div className={styles["letter__envelope-container"]}>
 				<div className={styles["letter__envelope-container__envelope"]}>
 					<img className={styles["letter__envelope-container__envelope__front"]} src={frontImg} alt="envelope" />
 					<img className={styles["letter__envelope-container__envelope__back"]} src={backImg} alt="envelope" />
-					<img className={styles["letter__envelope-container__envelope__flap"]} src={flapImg} alt="envelope" />
+					<motion.img className={styles["letter__envelope-container__envelope__flap"]} animate={envelopeControls} src={flapImg} alt="envelope" />
 				</div>
 			</div>
 		</div>
