1a154e3b166c5a77a1d251b4f9cfe61b61537871
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-10-13 21:49:12 +0200

    Test using process.env for the keys

--- src/home/components/section-contact/EmailVars.ts
+++ src/home/components/section-contact/EmailVars.ts
@@ -1,5 +1,5 @@
 export const EMAIL_VARS = {
-	EMAIL_KEY: "Nxvib6hyUFSl5mzua",
-	SERVICE_ID: "service_ayytesk",
-	TEMPLATE_ID: "template_jcqws8w",
+	EMAIL_KEY: process.env.EMAIL_KEY,
+	SERVICE_ID: process.env.SERVICE_ID,
+	TEMPLATE_ID: process.env.TEMPLATE_ID,
 };
