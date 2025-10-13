9879d57bda927c4297f48e3ab97f612f01723a43
WoutVanImpe <145569527+WoutVanImpe@users.noreply.github.com>
2025-10-13 18:55:18 +0200

    removed github secrets for email vars

--- src/home/components/section-contact/EmailVars.ts
+++ src/home/components/section-contact/EmailVars.ts
@@ -1,5 +1,5 @@
 export const EMAIL_VARS = {
-	EMAIL_KEY: import.meta.env.VITE_EMAIL_KEY,
-	SERVICE_ID: import.meta.env.VITE_SERVICE_ID,
-	TEMPLATE_ID: import.meta.env.VITE_TEMPLATE_ID,
+	EMAIL_KEY: "Nxvib6hyUFSl5mzua",
+	SERVICE_ID: "service_ayytesk",
+	TEMPLATE_ID: "template_jcqws8w",
 };
